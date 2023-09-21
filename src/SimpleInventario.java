e4db54abd0c355e115289a35082c5bd5c5dd11ef
Laboratorio 109 Pc-12 <lab-109-inf-12>
2023-09-21 19:38:45 -0400

    Nueva clase agregada

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
 public class SimpleInventario {
-    public static void  agregarArticulo() {
-        Scanner scaner = new scanner(System.in)
-        {
-        }
+    public static void agregarArticulo(String s) {
+        System.out.println("agregar un articulo: ");
+
+
+    }
+
+    public static void RemoverArticulo(int id) {
+    }
+
+    public static double obtenerPrecioArticulo(int id) {
+        return 0.00;
+    }
+
+    public static void ImprimiendoInventario() {
+    System.out.println("Ingrese una opción");
+    }
+
+    public static void main(String[] args) {
+     ImprimiendoInventario();
+
+    }
+
+}
+
+
+
