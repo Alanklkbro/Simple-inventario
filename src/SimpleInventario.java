7e8bd7a4f4021e2ffa207c2c1e8b7dc0050f837f
Laboratorio 109 Pc-11 <lab-109-inf-11>
2023-11-02 19:31:12 -0400

    Arreglo incluido para almacenar varios elementos

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,58 +1,28 @@
 import java.util.Scanner;
 
 public class SimpleInventario {
-    public static void agregarArticulo(String s) {
-        if (s.length() > 4) {
-            System.out.println("Artículo agregado exitosamente!");
-        } else {
-            System.out.println("No se permite este artículo");
+    static String[] articulos = new String[5];
+
+    public static void agregarArticulo() {
+        for (int i = 0; i < articulos.length; i++) {
+            articulos[i] = obtenerNombredeArticulo();
         }
     }
 
-    public static void removerArticulo(int id) {
-        if (id > 0) {
-            // Coloca aquí la lógica para remover el artículo
-            System.out.println("Artículo removido exitosamente!");
-        }
-    }
-
-    public static double obtenerPrecioArticulo(String articulo) {
-        double random = (Math.random() * 4900) + 100;
-        return random;
-    }
-
-    public static void modificarArticulo() {
-        String nombrearticulo = obtenerNombredeArticulo();
-        if (nombrearticulo.startsWith("A") || nombrearticulo.toUpperCase().startsWith("D")) {
-            double precio = obtenerPrecioArticulo(nombrearticulo);
-            if (precio >= 150 && precio <= 250) {
-                precio *= 1.02;
-                System.out.println("El precio del artículo " + nombrearticulo + " ha sido actualizado a " + precio);
-            } else if (precio > 250 && precio <= 500) {
-                precio *= 1.08;
-                System.out.println("El precio del artículo " + nombrearticulo + " ha sido actualizado a " + precio);
-            } else if (precio > 500) {
-                precio *= 1.12;
-                System.out.println("El precio del artículo " + nombrearticulo + " ha sido actualizado a " + precio);
-            }
-        } else if (nombrearticulo.toUpperCase().startsWith("CO") || nombrearticulo.toUpperCase().startsWith("M")) {
-            double precio = obtenerPrecioArticulo(nombrearticulo);
-            precio *= 0.8;
-            System.out.println("El precio del artículo " + nombrearticulo + " ha sido actualizado a " + precio);
-        }
-    }
-
-    public static void ImprimiendoInventario() {
-        System.out.println("Ingrese su nombre");
+    public static void imprimirInventario() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
         String usuario = scanner.next();
-        System.out.println("Bienvenido " + usuario);
-
-        modificarArticulo();
+        System.out.println("Bienvenid@ " + usuario);
+        System.out.println("Articulos registrados: ");
+        for (String articulo : articulos) {
+            System.out.println(articulo);
+        }
     }
 
     public static void main(String[] args) {
-        ImprimiendoInventario();
+        agregarArticulo();
+        imprimirInventario();
     }
 
     public static String obtenerNombredeArticulo() {
